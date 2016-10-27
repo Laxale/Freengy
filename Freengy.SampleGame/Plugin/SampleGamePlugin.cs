dfb49974bef1792a2c73c98451bd04f6affab438
Laxale <axel.1777@mail.ru>
2016-10-27 11:40:11 +0300

    Used string instead of ImageSource in SampleGamePlugin

--- Freengy.SampleGame/Plugin/SampleGamePlugin.cs
+++ Freengy.SampleGame/Plugin/SampleGamePlugin.cs
@@ -6,13 +6,19 @@
 namespace Freengy.SampleGame.Plugin 
 {
     using System;
-    
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+
+    using Freengy.Base.Interfaces;
     using Freengy.SampleGame.Views;
     using Freengy.GamePlugin.Interfaces;
-    
+
     using Res = Freengy.GamePlugin.Resources;
 
-
+    
+    /// <summary>
+    /// Sample/simple game plugin for testing
+    /// </summary>
     public class SampleGamePlugin : IGamePlugin 
     {
         public Guid Id { get; } = Guid.NewGuid();
@@ -23,6 +29,19 @@ namespace Freengy.SampleGame.Plugin
 
         public string GameIconSource { get; } = Res.DefaultGameIconSource;
 
+        //public ImageSource GameIconSource { get; } = GetDefaultIconSource();
+
         public string DisplayedName { get; } = Res.DefaultDisplayedGameName;
+
+
+        private static ImageSource GetDefaultIconSource()
+        {
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(Res.DefaultGameIconSource);
+            bitmapImage.EndInit();
+
+            return bitmapImage;
+        }
     }
 }
