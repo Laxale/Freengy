d8b0cb4b5fab5aa1cde952c37f0d0b1409abb479
Laxale <axel.1777@mail.ru>
2016-10-27 15:49:47 +0300

    Added MessageLoadGameRequest

---
+++ Freengy.GamePlugin/Messages/MessageLoadGameRequest.cs
@@ -0,0 +1,29 @@
+﻿// Created by Laxale 27.10.2016
+//
+//
+
+
+namespace Freengy.GamePlugin.Messages 
+{
+    using Freengy.Base.Messages;
+    using Freengy.Base.Interfaces;
+    using Freengy.GamePlugin.Interfaces;
+
+
+    /// <summary>
+    /// Someone requests to load a game
+    /// TODO: maybe add request source to filter bad requests?
+    /// </summary>
+    public class MessageLoadGameRequest : MessageBase 
+    {
+        private readonly IGamePlugin gamePlugin;
+        private readonly IUserAccount friendToInvite;
+
+        
+        public MessageLoadGameRequest(IGamePlugin gamePlugin, IUserAccount friendToInvite) 
+        {
+            this.gamePlugin = gamePlugin;
+            this.friendToInvite = friendToInvite;
+        }
+    }
+}
