f3874ad533717be617bee3829619caa14d592cc1
Laxale <axel.1777@mail.ru>
2016-11-14 06:44:22 +0300

    Added empty wizard base things

---
+++ Freengy.Base/Wizard/ViewModels/WizardPageViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿// Created by Laxale 14.11.2016
+//
+//
+
+
+namespace Freengy.Base.Wizard.ViewModels 
+{
+    using System;
+
+    using Freengy.Base.ViewModels;
+
+
+    public class WizardPageViewModelBase : WaitableViewModel 
+    {
+        protected override void SetupCommands() 
+        {
+            
+        }
+    }
+}
---
+++ Freengy.Base/Wizard/Views/WizardWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿// Created by Laxale 14.11.2016
+//
+//
+
+
+namespace Freengy.Base.Wizard.Views 
+{
+    using System.Windows;
+
+
+    public partial class WizardWindow : Window 
+    {
+        public WizardWindow() 
+        {
+            this.InitializeComponent();
+        }
+    }
+}
