22a3d734bc34d4584fd0afbfd9a3c409c7a08061
Laxale <axel.1777@mail.ru>
2018-04-22 06:40:20 +0300

    24

---
+++ Modules/Freengy.Friendlist/Views/FriendListView.xaml.cs
@@ -0,0 +1,19 @@
+﻿// Created by Laxale 23.10.2016
+//
+//
+
+using Freengy.Base.Attributes;
+using Freengy.FriendList.ViewModels;
+
+
+namespace Freengy.FriendList.Views 
+{
+    [HasViewModel(typeof(FriendListViewModel))]
+    public partial class FriendListView 
+    {
+        public FriendListView() 
+        {
+            InitializeComponent();
+        }
+    }
+}
