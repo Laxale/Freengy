1add6464711ab91cc5b71085fe46782682cd0d89
Laxale <axel.1777@mail.ru>
2018-05-08 21:32:03 +0300

    Tooltips visual rework

--- Freengy.UI/ViewModels/HeaderToolbarViewModel.cs
+++ Freengy.UI/ViewModels/HeaderToolbarViewModel.cs
@@ -9,6 +9,8 @@ using Freengy.Base.Messages;
 using Freengy.Base.Messages.Notification;
 using Freengy.Base.ViewModels;
 using Freengy.CommonResources.Styles;
+using Freengy.Networking.Constants;
+using Freengy.Networking.Interfaces;
 
 
 namespace Freengy.UI.ViewModels 
@@ -19,10 +21,13 @@ namespace Freengy.UI.ViewModels
     internal class HeaderToolbarViewModel : WaitableViewModel 
     {
         private bool isServerOnline;
+        private string serverAddress;
 
 
         public HeaderToolbarViewModel() 
         {
+            ServerAddress = Url.Http.RootUrl;
+
             this.Subscribe<MessageServerOnlineStatus>(OnServerStatusInform);
         }
 
@@ -39,6 +44,23 @@ namespace Freengy.UI.ViewModels
                 if (isServerOnline == value) return;
 
                 isServerOnline = value;
+
+                OnPropertyChanged();
+            }
+        }
+        
+        /// <summary>
+        /// Gets current server address.
+        /// </summary>
+        public string ServerAddress 
+        {
+            get => serverAddress;
+
+            private set
+            {
+                if (serverAddress == value) return;
+
+                serverAddress = value;
 
                 OnPropertyChanged();
             }
